366afa5ded01f54b637a3f5e3dcf422b2adbed0a
pk <fakemoney600@gmail.com>
2024-05-31 19:47:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('parsed');
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.header-images img');
+    let currentIndex = 0;
+
+    function switchImage() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('active');
+    }
+
+    setInterval(switchImage, 5000); 
 });
